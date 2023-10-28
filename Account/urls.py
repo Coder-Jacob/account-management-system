5e857f88c9bdaf9d118f025f70ee250fb3125258
Jacob <80373935+Jaacob01@users.noreply.github.com>
2023-10-28 17:29:22 +0800

    优化

---
+++ Account/urls.py
@@ -0,0 +1,38 @@
+"""Account URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import re_path as url
+# from django.views.generic.base import RedirectView
+# from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.static import serve as STATIC_SERVER
+
+import accountApp.views
+from . import settings
+from django.urls import path
+
+admin.site.site_header = 'VaultKey'
+admin.site.site_title = 'VaultKey账号管理系统'
+
+urlpatterns = [
+    path('admin/accountApp/accountlist', accountApp.views.AccountList),
+    path('', admin.site.urls),
+    path('account/', include('accountApp.urls')),
+    url('^static/(?P<path>.*)$', STATIC_SERVER, {'document_root': settings.STATIC_ROOT, 'show_indexes': True}, name='static'),
+    # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
+
+
+]
--- backend/demo.py
+++
@@ -1,5 +0,0 @@
-rows = 5  # 更改这个数字以调整三角形的大小
-for i in range(rows, 0, -1):
-    for j in range(i):
-        print('*', end='')
-    print()

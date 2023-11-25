eeb678999657772c713165c2e4229043afa7521f
jacob <spider1123@126.>
2023-11-25 19:43:11 +0800

    优化

--- AMS_Django/settings.py
+++ AMS_Django/settings.py
@@ -153,7 +153,7 @@ JWT_EXPIRED_DELTA = datetime.timedelta(hours=1)
 
 # SIMPLEUI 配置
 SIMPLEUI_STATIC_OFFLINE = True
-SIMPLEUI_LOGO = '/static/img/logo.png'
+SIMPLEUI_LOGO = '/img/logo.png'
 # 隐藏右侧SimpleUI广告链接和使用分析
 SIMPLEUI_HOME_INFO = False
 SIMPLEUI_ANALYSIS = False
